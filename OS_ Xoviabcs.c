14cbd5d595ebd7ec9e8e3dba2b419f628f05b3cd
dunnywei <wei_don@yahoo.com>
2019-11-02 20:46:24 -0400

    Lecture 3

--- OS_ Xoviabcs.c
+++ OS_ Xoviabcs.c
@@ -1,4 +1,5 @@
 ->good links->https://github.com/Developer-Y/cs-video-courses#computer-graphics
 Lecture  2->https://www.youtube.com/watch?v=k51934LHz3k
 (1:02)
-(7:15)
+(7:15)->done
+Lecture 3->https://www.youtube.com/watch?v=mio9OsckCZM&list=PLEJxKK7AcSEGPOCFtQTJhOElU44J_JAun&index=2
